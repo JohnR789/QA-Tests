c46368f94c075adcb949d29dfbed29361c8897fa
John <rollinsj789@gmail.com>
2024-09-09 09:51:44 -0400

    Update test1finale.js

--- test1finale.js
+++ test1finale.js
@@ -24,10 +24,8 @@ async function test1b(){
     // Variables
  
     url ="https://paul.overnightbdc.com/";
-    username = "paul.rollins";
-    //Jrmrkr7799
-    //Jrmrkr0699@
-    password = "Jrmrkr7799";
+    username = "";
+    password = ";
     firstname = "joe";
     lastname = "fisher";
     worktitle = "QA Automation Engineer";
